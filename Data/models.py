f781996a49cd1a1ed0c41e5b6b8854b851cb5009
MatthewFarer <matthewnfarer@gmail.com>
2026-05-30 18:18:34 +0300

    test

--- Data/models.py
+++ Data/models.py
@@ -119,20 +119,21 @@ class AddressBook(UserDict):
         if name in self.data:
             del self.data[name]
 
-    def upcoming_birthdays(self):
-        today = datetime.today().date()
-        birthdays = []
-        for record in self.data.values():
-            if record.birthday:
-                birthday = record.birthday.value
-                next_birthday = birthday.replace(
-                    year=today.year
-                    if birthday.replace(year=today.year) >= today
-                    else today.year + 1
-                )
-                if (next_birthday - today).days <= 7:
-                    birthdays.append(record)
-        return birthdays
+    def upcoming_birthdays(self, days=7):
+            """Return records with birthdays within the next `days` days (inclusive)."""
+            today = datetime.today().date()
+            birthdays = []
+            for record in self.data.values():
+                if record.birthday:
+                    birthday = record.birthday.value
+                    next_birthday = birthday.replace(
+                        year=today.year
+                        if birthday.replace(year=today.year) >= today
+                        else today.year + 1
+                    )
+                    if (next_birthday - today).days <= days:
+                        birthdays.append(record)
+            return birthdays
 
     def search(self, query):
         query = query.lower()
